15f731632232244ab535ff5038fcde2269787c07
Amr CHATELAIN <amr.chatelain@gmail.com>
2026-01-12 15:40:05 +0100

    done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -8,44 +10,96 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 
-
-
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
-
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async (req, res) => {
+  const { title, content } = req.body;
+  const newRecipe = await Recipe.create({ title, content });
+  res.json(newRecipe);
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res) => {
+  Recipe.find({})
+    .then((Recipes) => {
+      console.log(`Retrieved`, Recipes);
+      res.status(200).json(Recipes);
+    })
+    .catch((err) => {
+      res.status(500).json();
+    });
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
-
+app.get("/recipes/:id", (req, res) => {
+  Recipe.findById(req.params.id)
+    .then((recipe) => {
+      if (!recipe) {
+        return res.status(404).json({ message: "Recipe not found" });
+      }
+      res.status(200).json(recipe);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Failed to retrieve recipe" });
+    });
+});
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
-
+app.put("/recipes/:id", (req, res) => {
+  Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedRecipe) => {
+      if (!updatedRecipe) {
+        return res.status(404).json({ message: "Recipe not found" });
+      }
+      res.status(200).json(updatedRecipe);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Failed to update recipe" });
+    });
+});
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
-
-
+app.delete("/recipes/:id", (req, res) => {
+  Recipe.findByIdAndDelete(req.params.id)
+    .then((deletedRecipe) => {
+      if (!deletedRecipe) {
+        return res.status(404).json({ message: "Recipe not found" });
+      }
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Failed to delete recipe" });
+    });
+});
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
+// app.js
+//...
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+// ...
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
